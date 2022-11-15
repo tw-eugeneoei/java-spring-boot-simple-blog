2f76902d1163edda22799bf9392aea4c2a7c31e6
Eugene Oei <eugene.oei@thoughtworks.com>
2022-11-15 19:58:27 +0800

    added ResourceNotFoundException class

---
+++ src/main/java/com/example/simpleblog/exception/ResourceNotFoundException.java
@@ -0,0 +1,32 @@
+package com.example.simpleblog.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+// @ResponseStatus annotation cause speing boot to respond with the specified HTTP status code whenever this exception
+// is thrown from the controller
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+    private String resourceName;
+    private String fieldName;
+    private String fieldValue;
+
+    public ResourceNotFoundException(String resourceName, String fieldName, String fieldValue) {
+        super(String.format("%s not found with %s: '%s'", resourceName, fieldName, fieldValue)); // eg: Post not found with id: 1
+        this.resourceName = resourceName;
+        this.fieldName = fieldName;
+        this.fieldValue = fieldValue;
+    }
+
+    public String getFieldName() {
+        return fieldName;
+    }
+
+    public String getResourceName() {
+        return resourceName;
+    }
+
+    public String getFieldValue() {
+        return fieldValue;
+    }
+}
