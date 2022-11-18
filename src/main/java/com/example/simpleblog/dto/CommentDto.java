c7f3ad71e90d937213bd90376bcdfbeb36329c65
Eugene Oei <eugene.oei@thoughtworks.com>
2022-11-18 16:15:49 +0800

    created commentRepository and commentDto

---
+++ src/main/java/com/example/simpleblog/dto/CommentDto.java
@@ -0,0 +1,15 @@
+package com.example.simpleblog.dto;
+
+import com.example.simpleblog.entity.Post;
+import lombok.Data;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Data
+public class CommentDto {
+    private UUID id;
+    private String content;
+    private Post post;
+    private Date created_at;
+}
---
+++ src/main/java/com/example/simpleblog/repository/CommentRepository.java
@@ -0,0 +1,9 @@
+package com.example.simpleblog.repository;
+
+import com.example.simpleblog.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface CommentRepository extends JpaRepository<Comment, UUID> {
+}
