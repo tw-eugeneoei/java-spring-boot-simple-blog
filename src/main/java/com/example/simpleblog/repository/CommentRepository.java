c6fc3e58ed818de8add3611da56d27588b2fc251
Eugene Oei <eugene.oei@thoughtworks.com>
2022-11-21 17:12:57 +0800

    added @Transactional to deleteByIdAndPostId method in CommentRepository

--- src/main/java/com/example/simpleblog/repository/CommentRepository.java
+++ src/main/java/com/example/simpleblog/repository/CommentRepository.java
@@ -4,11 +4,16 @@ import com.example.simpleblog.entity.Comment;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
 public interface CommentRepository extends JpaRepository<Comment, UUID> {
     Page<Comment> findCommentsByPostId(UUID postId, Pageable pageable);
     Comment findByIdAndPostId(UUID commentId, UUID postId);
+
+    // Persisting and deleting objects in JPA requires a transaction. That's why we should use a @Transactional
+    // annotation when using these derived delete queries, to make sure a transaction is running.
+    @Transactional
     Long deleteByIdAndPostId(UUID commentId, UUID postId);
 }
