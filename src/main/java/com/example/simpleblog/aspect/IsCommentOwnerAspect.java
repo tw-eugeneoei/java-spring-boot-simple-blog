ba4ef9918c427afa8a145b45b3363db961413779
eugeneoei <eugeneoei89@gmail.com>
2022-12-10 19:14:57 +0800

    implment reuse of pointcut expression

--- src/main/java/com/example/simpleblog/aspect/IsCommentOwnerAspect.java
+++ src/main/java/com/example/simpleblog/aspect/IsCommentOwnerAspect.java
@@ -3,8 +3,10 @@ package com.example.simpleblog.aspect;
 import com.example.simpleblog.entity.Comment;
 import com.example.simpleblog.repository.CommentRepository;
 import com.example.simpleblog.repository.UserRepository;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
@@ -17,9 +19,26 @@ public class IsCommentOwnerAspect extends IsResourceOwnerAspect<Comment> {
         super(commentRepository, userRepository, "Comment");
     }
 
-    @Before(value = "@annotation(isCommentOwner) && args(postId, commentId, ..)", argNames = "isCommentOwner, postId, commentId")
+//    @Before(value = "@annotation(isCommentOwner) && args(postId, commentId, ..)", argNames = "isCommentOwner, postId, commentId")
+//    public void updateAndDeletePostAuthorisationAdvice(IsCommentOwner isCommentOwner, UUID postId, UUID commentId) {
+//        UUID commentOwnerId = getResource(commentId).getUser().getId();
+//        validateOwnership(commentOwnerId, isCommentOwner.method());
+//    }
+
+    @Pointcut(
+            value = "@annotation(isCommentOwner) && args(postId, commentId, ..)",
+            argNames = "isCommentOwner, postId, commentId"
+    )
+    public void updateAndDeleteAdvice(IsCommentOwner isCommentOwner, UUID postId, UUID commentId) {}
+
+    @Before("updateAndDeleteAdvice(isCommentOwner, postId, commentId)")
     public void updateAndDeletePostAuthorisationAdvice(IsCommentOwner isCommentOwner, UUID postId, UUID commentId) {
         UUID commentOwnerId = getResource(commentId).getUser().getId();
         validateOwnership(commentOwnerId, isCommentOwner.method());
+    }
+
+    @After("updateAndDeleteAdvice(isCommentOwner, postId, commentId)")
+    public void afterUpdateAndDeletePostAuthorisationAdvice(IsCommentOwner isCommentOwner, UUID postId, UUID commentId) {
+        System.out.println("AFTER ADVICE => Reusing pointcut expression");
     }
 }
